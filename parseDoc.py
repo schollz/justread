ce8f90ddda186bfae83910d1d88089d350d6292c
Zack Scholl <zack.scholl@gmail.com>
2015-11-12 11:00:11 -0500

    Improved a bit

--- parseDoc.py
+++ parseDoc.py
@@ -56,7 +56,20 @@ def getDoc(url):
                 endTag = '</p>'
             try:
                 newString = startTag + " ".join(str(child.text_content().encode('utf-8')).split()) + endTag + "\n"
-                if len(newString) > 50000 or len(newString)<14 or '{ "' in newString or '{"' in newString or "function()" in newString or 'else {' in newString or '.js' in newString or 'ajax' in newString or 'var ' in newString or '()' in newString:
+                if (len(newString) > 50000 or 
+                        len(newString)<14 or 
+                        '{ "' in newString or 
+                        '{"' in newString or 
+                        "function()" in newString or 
+                        'else {' in newString or 
+                        '.js' in newString or 
+                        'ajax' in newString or 
+                        'var ' in newString or 
+                        ('Advertisement' in newString and len(newString)<200) or 
+                        'Continue reading' in newString or 
+                        ('Photo' in newString and 'Credit' in newString) or 
+                        'window.' in newString or 
+                        '()' in newString):
                     continue
                 #print(len(newString))
                 if len(newString) > 200 and len(newString) < 1000 and 'li' not in tag:
@@ -94,6 +107,7 @@ def getDoc(url):
     docString = docString.decode('utf-8')
     fileSize = 0.7 + float(sys.getsizeof(docString)/1000.0)
     fileSize = round(fileSize,1)
+    print(docString)
     return {'title':title,'description':description,'url':url,'timeElapsed':timeElapsed,'content':docString,'size':fileSize}
 
 #print(getDoc('http://www.bbc.co.uk/news/entertainment-arts-34768201'))
